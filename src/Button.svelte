467679feffb8f3dbab0cf102b1b16ea7ede24c7f
Friedel Baumann <60962667+FlBaumann@users.noreply.github.com>
2024-09-11 10:37:02 +0200

    fix tab size

--- src/Button.svelte
+++ src/Button.svelte
@@ -1,40 +1,40 @@
 <script lang="ts">
-    import type { Editor } from "@tiptap/core";
+  import type { Editor } from "@tiptap/core";
 
-    export let editor: Editor;
-    export let key: string;
-    export let disabled: boolean;
-    export let activeButtons: string[] = [];
-    export let action: Function;
+  export let editor: Editor;
+  export let key: string;
+  export let disabled: boolean;
+  export let activeButtons: string[] = [];
+  export let action: Function;
 
-    function handleClick() {
-        action(editor);
+  function handleClick() {
+    action(editor);
 
-        if (activeButtons.includes(key)) {
-            activeButtons = activeButtons.filter((k) => k !== key);
-        } else {
-            activeButtons = [...activeButtons, key];
-        }
+    if (activeButtons.includes(key)) {
+      activeButtons = activeButtons.filter((k) => k !== key);
+    } else {
+      activeButtons = [...activeButtons, key];
     }
+  }
 </script>
 
 <!-- ############################### <HTML> ############################## -->
 
 <button
-    {disabled}
-    class:active={activeButtons.includes(key)}
-    on:click={() => handleClick()}>{key}</button
+  {disabled}
+  class:active={activeButtons.includes(key)}
+  on:click={() => handleClick()}>{key}</button
 >
 
 <!-- ############################## </HTML> ############################## -->
 
 <style>
-    button {
-        margin: 0;
-    }
+  button {
+    margin: 0;
+  }
 
-    button.active {
-        background: black;
-        color: white;
-    }
+  button.active {
+    background: black;
+    color: white;
+  }
 </style>
--- src/Toolbar.svelte
+++ src/Toolbar.svelte
@@ -1,42 +1,42 @@
 <script lang="ts">
-    import type { Editor } from "@tiptap/core";
-    import type { Action } from "./types/Action";
-    import Button from "./Button.svelte";
-    import ActionDefinitions from "./ActionDefinitions";
+  import type { Editor } from "@tiptap/core";
+  import type { Action } from "./types/Action";
+  import Button from "./Button.svelte";
+  import ActionDefinitions from "./ActionDefinitions";
 
-    export let editor: Editor;
-    export let toolbar: string[];
-    export let disabled: boolean;
-    export let activeButtons: string[] = [];
+  export let editor: Editor;
+  export let toolbar: string[];
+  export let disabled: boolean;
+  export let activeButtons: string[] = [];
 
-    function getConfiguredToolbarActions(): Action[] {
-        return ActionDefinitions.getActions().filter((action: Action) => {
-            return toolbar.includes(action.key);
-        });
-    }
+  function getConfiguredToolbarActions(): Action[] {
+    return ActionDefinitions.getActions().filter((action: Action) => {
+      return toolbar.includes(action.key);
+    });
+  }
 </script>
 
 <!-- ############################### <HTML> ############################## -->
 
 {#if editor}
-    <div class="toolbar">
-        {#each getConfiguredToolbarActions() as action}
-            <Button
-                {editor}
-                {disabled}
-                {activeButtons}
-                key={action.key}
-                action={action.buttonAction}
-            />
-        {/each}
-    </div>
+  <div class="toolbar">
+    {#each getConfiguredToolbarActions() as action}
+      <Button
+        {editor}
+        {disabled}
+        {activeButtons}
+        key={action.key}
+        action={action.buttonAction}
+      />
+    {/each}
+  </div>
 {/if}
 
 <!-- ############################## </HTML> ############################## -->
 
 <style>
-    .toolbar {
-        background: #ccc;
-        padding: 2px;
-    }
+  .toolbar {
+    background: #ccc;
+    padding: 2px;
+  }
 </style>
