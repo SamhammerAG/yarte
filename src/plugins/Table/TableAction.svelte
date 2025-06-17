1d88977cbcbaf9b8b8b494a0a06487df126378ac
Nils Polauer <56922874+npolauer@users.noreply.github.com>
2025-06-17 12:39:09 +0200

    AID-26743 add input fields to table button dropdown to generate bigger tables

--- src/plugins/Table/TableAction.svelte
+++ src/plugins/Table/TableAction.svelte
@@ -13,8 +13,8 @@
   };
 
   let dropdownOpen = $state(false);
-  let xPos: number = $state(0);
-  let yPos: number = $state(0);
+  let rows: number = $state(0);
+  let columns: number = $state(0);
 
   const tableGridSize: number = 10;
 
@@ -31,25 +31,31 @@
       {#each { length: tableGridSize }, x}
         {#each { length: tableGridSize }, y}
           <button
-            class:highlight={xPos >= x && yPos >= y}
-            onmouseenter={() => ((xPos = x), (yPos = y))}
-            onclick={() => createTable(x + 1, y + 1)}
-            aria-label="Create table with {x + 1} rows and {y + 1} columns"
+            class:highlight={rows >= x + 1 && columns >= y + 1}
+            onmouseenter={() => ((rows = x + 1), (columns = y + 1))}
+            onclick={() => createTable(rows, columns)}
+            aria-label="Create table with {rows} rows and {columns} columns"
           ></button>
         {/each}
       {/each}
     </div>
-    <div class="display">{xPos + 1} x {yPos + 1}</div>
+    <div class="table-toolbar">
+      <input name="rows" type="number" bind:value={rows} min="0" max="999" />
+      <span>x</span>
+      <input name="columns" type="number" bind:value={columns} min="0" max="999" />
+      <button type="button" onclick={() => createTable(rows, columns)}>OK</button>
+    </div>
   </DropdownButton>
 {/if}
 
 <style>
   .table {
     display: grid;
+    width: 10rem;
     grid-template-columns: repeat(10, 1fr);
     grid-template-rows: repeat(10, 1fr);
 
-    & button {
+    button {
       border-color: var(--font-light-color, #eee);
       border-style: solid;
       border-width: 0 1px 1px 0;
@@ -65,9 +71,38 @@
     }
   }
 
-  .display {
-    text-align: center;
-    color: var(--yarte-bg-icon, black);
-    padding-bottom: 0.25rem;
+  .table-toolbar {
+    display: grid;
+    width: 10rem;
+    grid-template-columns: repeat(4, 1fr);
+    margin-top: 0.25rem;
+
+    input {
+      width: 3rem;
+      text-align: center;
+      box-sizing: border-box;
+      appearance: default;
+
+      /* Firefox */
+      -moz-appearance: textfield;
+
+      /* Chrome, Safari, Edge, Opera */
+      &::-webkit-outer-spin-button,
+      &::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+      }
+    }
+
+    span {
+      width: 1rem;
+      text-align: center;
+    }
+
+    button {
+      width: 2.75rem;
+      text-align: center;
+      margin-left: 0.25rem;
+    }
   }
 </style>
