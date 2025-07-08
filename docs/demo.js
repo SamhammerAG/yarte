af1ca1f7dbd8f874348197e05ee93407d7957e07
Friedel Baumann <60962667+FlBaumann@users.noreply.github.com>
2025-07-08 09:14:32 +0200

    test demo page

---
+++ docs/demo.js
@@ -0,0 +1,8 @@
+import { Editor } from 'https://esm.sh/@tiptap/core'
+import StarterKit from 'https://esm.sh/@tiptap/starter-kit'
+
+export const editor = new Editor({
+  element: document.querySelector('#editor'),
+  extensions: [StarterKit],
+  content: '<p>Hello from Tiptap + Plain JS!</p>',
+})
